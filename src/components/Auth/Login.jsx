4b120a0ef190ab154d49552865af8ec1590e3d1f
Abhay Chauhan <abhaychauhan291205@gmail.com>
2025-07-13 01:09:20 +0530

    Add LogIn icon and update text color in Login component

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { LogIn,User, Lock, Mail} from "lucide-react";
+
 
 const Login = ({ handleLogin }) => {
   const [email, setEmail] = useState('');
@@ -15,6 +17,7 @@ const Login = ({ handleLogin }) => {
     <div className="h-screen w-screen flex items-center justify-center bg-black bg-gradient-to-br from-black via-gray-900 to-black">
       <div className="w-[90%] max-w-md backdrop-blur-md bg-white/10 border border-cyan-400/40 p-10 rounded-2xl shadow-2xl hover:shadow-cyan-500/40 transition-all duration-500">
         <h2 className="text-white text-3xl font-bold text-center mb-8">
+          <LogIn className="inline mr-2" />
            Login</h2>
 
         <form onSubmit={submitHandler} className="flex flex-col gap-5">
@@ -42,8 +45,8 @@ const Login = ({ handleLogin }) => {
             Log in
           </button>
 
-          <p className="text-center text-sm text-cyan-200">
-            Don't have Login credentials? <span className="text-cyan-400 hover:underline cursor-pointer">click here</span>
+          <p className="text-center text-sm text-red">
+            Don't have Login credentials? <span className="!text-cyan-200 hover:underline cursor-pointer">click here</span>
           </p>
         </form>
       </div>
